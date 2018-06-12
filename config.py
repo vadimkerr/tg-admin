9cd9a62e1fd4ef735d3645d074c95dd31adeeca1
Vadim Stepanov <vadimkerr@gmail.com>
2018-06-12 18:09:13 +0300

    add gitignore

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 API_TOKEN = '611696352:AAH7J26zRxyP96FnhujhjwZXoufVBKqOt5g'
 REQUEST_KWARGS = {
-    'proxy_url': 'https://138.68.236.23:3128'
+    'proxy_url': 'https://206.81.2.4:808'
 }
-# admin's internal telegram id
 ADMIN_ID = '142561490'
 GROUP_LINK = 't.me/lil_thug'
